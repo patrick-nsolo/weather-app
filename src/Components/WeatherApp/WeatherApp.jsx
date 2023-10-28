d1179282cb7817519d737887e88ff5a7b745079a
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-28 17:50:39 +0100

    update weather app location styling

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -26,14 +26,14 @@ function WeatherApp() {
       <div className='weather-location'>London</div>
       <div className='data-container'>
         <div className='element'>
-            <img src='' alt='' className='icon'/>
+            <img src={humidity_icon} alt='' className='icon'/>
             <div className='data'>
                 <div className='humidity-percentage'>64%</div>
                 <div className='text'>Humidity</div>
             </div>
         </div>
         <div className='element'>
-            <img src='' alt='' className='icon'/>
+            <img src={wind_icon} alt='' className='icon'/>
             <div className='data'>
                 <div className='humidity-percentage'>18 km/h</div>
                 <div className='text'>Wind Speed</div>
