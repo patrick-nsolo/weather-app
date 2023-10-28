6903f1fda0992fe3168d4b15ad2793591e0b318c
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-28 16:37:43 +0100

    update Weather App

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './WeatherApp.css';
 
+import search_icon from '../Assets/search.png';
+import clear_icon from '../Assets/clear.png';
+import cloud_icon from '../Assets/cloud.png';
+import drizzle_icon from '../Assets/drizzle.png';
+import rain_icon from '../Assets/rain.png';
+import snow_icon from '../Assets/snow.png';
+import wind_icon from '../Assets/wind.png';
+import humidity_icon from '../Assets/humidity.png';
+
 function WeatherApp() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className='container'>
+      <div className='top-bar'>
+        <input type='text' className='cityInput' placeholder='Enter City Name'/>
+      </div>
     </div>
   )
 }
