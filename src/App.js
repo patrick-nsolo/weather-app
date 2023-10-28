4e0b2a9ac1d6ff77250c0fcd4e9d2858c8ec4ca2
Patrick Nsolo <64790572+patrick-nsolo@users.noreply.github.com>
2023-10-28 16:26:22 +0100

    update weather app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import WeatherApp from './Components/WeatherApp/WeatherApp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <WeatherApp/>
     </div>
   );
 }
---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WeatherApp.css';
+
+function WeatherApp() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default WeatherApp
